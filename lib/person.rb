4f5f54466990b878bf8c55025118d0c4e716bad6
Vesna Vucinic <vesna.vucinic68@gmail.com>
2019-10-30 19:08:33 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,25 @@
 class Person
-  def initialize (first_name, last_name)
+  
+  def initialize(first_name, last_name)
     @first_name = first_name
     @last_name = last_name
   end
+  
+  def name=(full_name)
+    first_name, last_name = full_name.split
+    @first_name = first_name
+    @last_name = last_name
+  end
+  
+  def name
+     "#{@first_name} #{@last_name}".strip
+  end
+  
+  def job=(job_rolle)
+    @job = job_rolle
+  end
+  
+  def job 
+    @job
+  end
+end
